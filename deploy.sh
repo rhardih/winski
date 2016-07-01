364c21419d3005163a642a557ffae98b25094a34
René Hansen <renehh@gmail.com>
2016-07-01 14:21:39 +0200

    Updates deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,7 @@ npm run build
 
 awk -F, '{$1=$1+1;print}' OFS=, VERSION > VERSION.tmp && mv VERSION{.tmp,}
 
-git commit -a -m 'Creates new release'
+git commit -a -m "Creates release deploy #`cat VERSION`"
 git push
+
+git checkout master
